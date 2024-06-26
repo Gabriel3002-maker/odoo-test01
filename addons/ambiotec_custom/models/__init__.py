4d75afae0ebccfe96de807256a4a7a1481046627
Gabriel Octavio <gv8536892@gmail.com>
2024-06-26 14:36:53 -0500

    elimanated modules

--- addons/ambiotec_custom/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import stock_quant
-from . import purchase_order
-from . import hr_work_entry
--- addons/multiple_delivery_purchase/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import purchase_order
-from . import purchase_order_line
-from . import product_template
